7854c2cc20f95e58a7dec08eba9d348caa7bf1b4
anthony <anthonysa0813@gmail.com>
2022-12-29 10:45:19 -0500

    change the variables

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 const { createPool } = require("mysql2/promise");
 
 const pool = createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD || "D3s4rrollo2023",
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
+  host: "172.25.120.14",
+  user: "desarrollo",
+  password: "D3s4rrollo2023",
+  port: 3306,
+  database: "DESARROLLO",
 });
 
 module.exports = pool;
